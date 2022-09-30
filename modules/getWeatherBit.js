f8ec56a2f3d118cc15c6c082679f255efae9938f
Mathew Torres <torres.mathew@gmail.com>
2022-09-30 15:38:30 -0700

    add cache to project

--- modules/getWeatherBit.js
+++ modules/getWeatherBit.js
@@ -1,16 +1,33 @@
 'use strict';
 const axios = require('axios');
+const cache = require('./cache.js');
+
 
 async function getWeatherBit(request, response) {
   const lat = request.query.lat;
   const lon = request.query.lon;
   const searchQuery = request.query.searchQuery;
-  const weatherBitUrl = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHERBIT_API_KEY}`;
+  const weatherBitUrl = `https://api.weatherbit.io/v2.0/forecast/daily?${searchQuery}&lat=${lat}&lon=${lon}&key=${process.env.WEATHERBIT_API_KEY}`;
 
   try {
-    const weatherBitResponse = await axios.get(weatherBitUrl);
-    const weatherBitData = weatherBitResponse.data.data.map(day => new Weather(day));
-    response.status(200).send(weatherBitData);
+    const key = searchQuery + lat + lon + 'weatherbit';
+
+
+    if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
+      console.log('Cache hit');
+      response.status(200).send(cache[key].data);
+    } else {
+      console.log('Cache miss');
+      const weatherBitResponse = await axios.get(weatherBitUrl);
+      const weatherBitData = weatherBitResponse.data.data.map(day => new Weather(day));
+
+      cache[key] = {
+        data: weatherBitData,
+        timestamp: Date.now()
+      };
+      console.log('Cache is: ', cache);
+      response.status(200).send(weatherBitData);
+    }
   } catch (error) {
     response.status(500).send('Sorry, something went wrong');
   }
--- modules/movies.js
+++ modules/movies.js
@@ -1,15 +1,30 @@
 'use strict';
 
 const axios = require('axios');
+const cache = require('./cache.js');
 
 async function getMovies(request, response) {
   const searchQuery = request.query.searchQuery;
   const movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`;
 
   try {
-    const movieResponse = await axios.get(movieUrl);
-    const movieData = movieResponse.data.results.map(movie => new Movie(movie));
-    response.status(200).send(movieData);
+    const key = searchQuery + 'movie';
+
+    if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
+      console.log('Cache hit');
+      response.status(200).send(cache[key].data);
+    } else {
+      console.log('Cache miss');
+      const movieResponse = await axios.get(movieUrl);
+      const movieData = movieResponse.data.results.map(movie => new Movie(movie));
+
+      cache[key] = {
+        data: movieData,
+        timestamp: Date.now()
+      };
+      console.log('Cache is: ', cache);
+      response.status(200).send(movieData);
+    }
   } catch (error) {
     response.status(500).send('Sorry, something went wrong');
   }
